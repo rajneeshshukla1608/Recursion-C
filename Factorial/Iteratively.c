7bc81d831da409e9e32f62bc256d1ab0d3699144
Rajneesh Shukla <65530868+rajneeshshukla1608@users.noreply.github.com>
2021-07-28 01:29:33 +0530

    Create Iteratively.c

---
+++ Factorial/Iteratively.c
@@ -0,0 +1,23 @@
+/*
+Iterative method of factorial of any given number iteratively
+*/
+
+#include<stdio.h>
+#include<cs50.h>
+
+int factorial(int n){
+ int lap = 1, i;
+    for (i = 2; i <= n; i++)
+        lap *= i;
+    return res;
+}
+
+int main(void){
+    
+    int n, s;
+    scanf("%d", &n);
+
+    s = factorial(n);
+    printf(" your iterative method of finding the fctorial is %d", s);
+    
+}
