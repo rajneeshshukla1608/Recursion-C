52f9e42204a70e55daea64ac1bf204c048a97017
Rajneesh Shukla <65530868+rajneeshshukla1608@users.noreply.github.com>
2021-08-09 11:14:42 +0530

    Add files via upload

---
+++ EXPO.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int expo(int e){
+
+   int result;
+   if(e == 0){
+     return 1;
+   }else if(e%2==0){
+      return 2*expo(e-1);
+   }else{
+     result = expo(e/2);
+     return result*result*2;
+   }
+
+
+     /* if(e==0){
+      return 1;
+      }else{
+      return 2*expo(e-1);
+      }*/
+}
+
+int main(void){
+    int n;
+    scanf("%d",&n);
+   printf("%d", expo(n));
+   getch();
+
+    return 0;
+}
