c325cb97424a518215d55ec7aa360e518708e875
Rajneesh Shukla <65530868+rajneeshshukla1608@users.noreply.github.com>
2021-08-09 23:11:00 +0530

    Add files via upload

---
+++ SPELL.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+char arr[10][6] = {"Zero" , "One" , "Two",
+		   "Three", "Four", "Five",
+		   "Six", "Seven","Eight", "Nine"};
+
+void spell(int k){
+    int n;
+    n = k % 10;
+    if (k / 10 != 0)
+	spell(k / 10);
+    printf("%s  ",arr[n]);
+}
+
+
+int main(){
+   int n;
+   clrscr();
+   printf("Enter the number to find the spelling: ");
+   scanf("%d", &n);
+   spell(n);
+   getch();
+   return 0;
+
+
+}
